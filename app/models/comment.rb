ec80048b1cdf136b9b3b506fc3cb9bc147a6f997
Dennis O'Keeffe <hello@dennisokeeffe.com>
2022-04-07 08:04:51 +1000

    feat: ruby day 50 completed

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  post_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Comment < ApplicationRecord
+  belongs_to :post
+end
---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Post < ApplicationRecord
+end
